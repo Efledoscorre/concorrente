76d88514782879280c03b9363f632bc4967398c4
Caio Vinícius Rodrigues Moreira <caiobiscas@gmail.com>
2024-11-23 14:45:51 -0300

    implementando expresssão regular que identifica a chave do objeto onde a substring de busca foi encontrada

--- src/main/java/ucb/busca/servidores/testeAlgoritmos/SearchAlgorithm.java
+++ src/main/java/ucb/busca/servidores/testeAlgoritmos/SearchAlgorithm.java
@@ -1,6 +1,10 @@
 package ucb.busca.servidores.testeAlgoritmos;
 
+import java.util.regex.Pattern;
+
 public interface SearchAlgorithm {
+
+    Pattern pattern = Pattern.compile("\"(\\d+)\"");
 
     void buscaSubString(String text, String substring);
 
